205407f977916cf53d100affe1162290d01a9159
Brian Campbell <brian.d.campbell@gmail.com>
2017-09-18 07:48:53 -0600

    util method to generate a 2048 RSA key pair

--- src/main/java/b_c/unbearable/utils/RsaKeyUtil.java
+++ src/main/java/b_c/unbearable/utils/RsaKeyUtil.java
@@ -1,6 +1,9 @@
 package b_c.unbearable.utils;
 
+import java.security.GeneralSecurityException;
 import java.security.KeyFactory;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
 import java.security.NoSuchAlgorithmException;
 import java.security.interfaces.RSAPublicKey;
 import java.security.spec.InvalidKeySpecException;
@@ -11,12 +14,15 @@ import java.security.spec.RSAPublicKeySpec;
  */
 public class RsaKeyUtil
 {
+
+    public static final String RSA = "RSA";
+
     public static RSAPublicKey rsaPublicKey(byte[] modulus, byte[] publicExponent)
     {
         try
         {
             RSAPublicKeySpec rsaPublicKeySpec = new RSAPublicKeySpec(Util.bigInt(modulus), Util.bigInt(publicExponent));
-            KeyFactory kf = KeyFactory.getInstance("RSA");
+            KeyFactory kf = KeyFactory.getInstance(RSA);
             return (RSAPublicKey) kf.generatePublic(rsaPublicKeySpec);
         }
         catch (NoSuchAlgorithmException | InvalidKeySpecException e)
@@ -24,4 +30,13 @@ public class RsaKeyUtil
             throw new RuntimeException("Unable to create RSAPublicKey object from modulus & public exponent byte arrays", e);// TODO type
         }
     }
+
+    public static KeyPair generate2048RsaKeyPair() throws GeneralSecurityException
+    {
+        KeyPairGenerator kpg = KeyPairGenerator.getInstance(RSA);
+        kpg.initialize(2048);
+        return kpg.generateKeyPair();
+    }
+
+
 }
